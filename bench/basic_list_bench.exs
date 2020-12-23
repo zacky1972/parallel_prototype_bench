b5541540618da88477b296321d9ae6272cd947c3
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-23 10:10:29 +0900

    update BasicListBench

--- bench/basic_list_bench.exs
+++ bench/basic_list_bench.exs
@@ -5,5 +5,47 @@ defmodule BasicListBench do
   @list_1000 Enum.to_list(1..1000)
   @list_10000 Enum.to_list(1..10000)
 
-  
+  # bench "@list_1 ++ @list_1000" do
+  # 	@list_1 ++ @list_1000
+  # end
+
+  # bench "@list_1000 ++ @list_1" do
+  # 	@list_1000 ++ @list_1
+  # end
+
+  bench "@list_1 ++ @list_10000" do
+  	@list_1 ++ @list_10000
+  end
+
+  bench "@list_10000 ++ @list_1" do
+  	@list_10000 ++ @list_1
+  end
+
+  bench "@list_1000 ++ @list_10000" do
+  	@list_1000 ++ @list_10000
+  end
+
+  bench "@list_10000 ++ @list_1000" do
+  	@list_10000 ++ @list_1000
+  end
+
+  # bench "@list_10000 ++ @list_10000" do
+  # 	@list_10000 ++ @list_10000
+  # end
+
+  bench "Enum.count(@list_1000)" do
+  	Enum.count(@list_1000)
+  end
+
+  bench "Enum.count(@list_10000)" do
+  	Enum.count(@list_10000)
+  end
+
+  bench "Enum.reverse(@list_1000)" do
+  	Enum.reverse(@list_1000)
+  end
+
+  bench "Enum.reverse(@list_10000)" do
+  	Enum.reverse(@list_10000)
+  end
 end
