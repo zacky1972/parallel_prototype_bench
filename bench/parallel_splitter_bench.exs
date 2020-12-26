779014a15112951b94ecc326665096652fefb5e4
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-26 18:02:12 +0900

    remove default value of arg

--- bench/parallel_splitter_bench.exs
+++ bench/parallel_splitter_bench.exs
@@ -4,7 +4,7 @@ defmodule ParallelSplitterBench do
   @list_10000 Enum.to_list(1..10000)
 
   bench "ParallelSplitter(@list_10000, 1000)" do
-  	ParallelSplitter.split({SplitterHelper, :count}, self(), 0, @list_10000, 1000, [:monitor])
+  	ParallelSplitter.split({SplitterHelper, :count}, self(), 0, @list_10000, 1000, [], [:monitor])
   	|> Enum.map(fn {_, id} -> id end)  	
   end
 end
