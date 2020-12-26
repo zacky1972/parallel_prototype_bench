59a443b82acaf090d4de82c5958dba395969d28a
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-26 20:05:18 +0900

    create ParallelPrototypeBBench

---
+++ bench/parallel_prototype_bench.exs
@@ -0,0 +1,21 @@
+defmodule ParallelPrototypeBBench do
+  use Benchfella
+
+  @range_0x10000 1..0x10000
+
+  bench "Enum.map(@range_0x10000, & &1 * 2)" do
+  	Enum.map(@range_0x10000, & &1 * 2)
+  end
+
+  bench "ParallelPrototype.pmap(@range_0x10000, & &1 * 2)" do
+  	ParallelPrototype.pmap(@range_0x10000, & &1 * 2)
+  end
+
+  bench "Enum.map(@range_0x10000, logistic_map_10)" do
+  	Enum.map(@range_0x10000, &LogisticMap.logistic_map_10(&1))
+  end
+
+  bench "ParallelPrototype.pmap(@range_0x10000, logistic_map_10)" do
+  	ParallelPrototype.pmap(@range_0x10000, &LogisticMap.logistic_map_10(&1))
+  end
+end
